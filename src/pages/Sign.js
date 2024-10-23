e95c64d9c5c0258fee4f5c9ce9323e88c6ae3603
Joseph nkansah <133186186+Josephnkansah@users.noreply.github.com>
2024-10-23 04:59:44 +0000

    skill academy

---
+++ src/pages/Sign.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+import Login from '../components/Login/Login'
+
+const Sign = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Login/>
+        </div>
+  )
+}
+
+export default Sign
