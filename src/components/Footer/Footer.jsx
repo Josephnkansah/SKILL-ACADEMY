03b41007066c2c28b7a1ad6d058238c4c5761592
Joseph nkansah <133186186+Josephnkansah@users.noreply.github.com>
2024-10-22 17:42:29 -1200

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,30 +6,34 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 const Footer = () => {
   return (
     <footer className="footer">
-     <div className="footer-content">
-      <p>© 2024 SKILL ACADEMY</p>
-      <ul className="footer-link">
-        <li><a href="/terms-of-service">Terms of Service</a></li>
+   <div className="footer-content">
+       <p>© 2024 SKILL ACADEMY</p>
+     <ul className="footer-link">
         <li><a href="/privacy-policy">Privacy Policy</a></li>
+        <li><a href="/terms-of-service">Terms of Service</a></li>
+        
       </ul>
     </div>
     
     <div className="social">
-         <a href="https://www.facebook.com/share/Z8GqSauz4JMquVDu/?mibextid=LQQJ4d" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faFacebook} />
+             <a href="mailto:jnkansahoseph@gmail.com">
+        <FontAwesomeIcon icon={faEnvelope} />
          </a>
+             <a href="https://www.instagram.com/youngma_90/profilecard/?igsh=M2p2bnQxOWtteWIx" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faInstagram} />
+      </a>
+
         <a href="https://twitter.com/Josephnkansah" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faTwitter} />
       </a>
-       <a href="https://www.instagram.com/youngma_90/profilecard/?igsh=M2p2bnQxOWtteWIx" target="_blank" rel="noopener noreferrer">
-        <FontAwesomeIcon icon={faInstagram} />
-      </a>
-       <a href="mailto:jnkansahoseph@gmail.com">
-        <FontAwesomeIcon icon={faEnvelope} />
+               <a href="https://www.facebook.com/share/Z8GqSauz4JMquVDu/?mibextid=LQQJ4d" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faFacebook} />
          </a>
+
+
      </div>
   </footer>
-  );
-};
+  )
+}
 
-export default Footer
+export default Footer;
