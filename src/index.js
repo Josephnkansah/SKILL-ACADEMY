ceb02d94fba41b60d7bc1fc1cd89c06f751ef2ff
Joseph nkansah <133186186+Josephnkansah@users.noreply.github.com>
2024-10-22 17:04:19 -1200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,11 @@ const router = createBrowserRouter([
     path: "/",
     element: <App/>,
   },
-{
+  {
+      path: "Sign",
+      element: <Sign/>,
+        },
+  {
   path: "Contact",
   element: <Contact/>,
 },
